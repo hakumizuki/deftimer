acc2e0134045bb1190040f2d36f885bf2b2f6fe2
Taichi Masuyama <montanha.masu536@gmail.com>
2021-06-16 11:15:32 +0900

    setup

---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import pathlib
+
+
+# Get the long description from the README file
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name='decotimer',
+    version='0.0.1',
+    description='A very simple Python tool for process inspection and timer. (alpha)',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/hakumizuki/decotimer',
+    author='Taichi Masuyama',
+    author_email='montanha.masu536@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    keywords='python, algorithms, timer, tools',
+    package_dir={'': 'decotimer'},
+    packages=find_packages(where='decotimer'),
+    python_requires='>=3.6, <4',
+    install_requires=['pathlib==1.0.1'],
+    extras_require={
+        'dev': [],
+        'test': ['flake8'],
+    },
+    project_urls={
+        'Bug Reports': 'https://github.com/hakumizuki/decotimer/issues',
+        'Source': 'https://github.com/hakumizuki/decotimer/',
+    },
+)
